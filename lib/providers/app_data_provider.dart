dc31fe30d5fde7b03e0d285f1d0648e1d1f36197
acer <sinhle.dev@gmail.com>
2023-10-03 20:31:27 +0700

    03/10/23

--- lib/providers/app_data_provider.dart
+++ lib/providers/app_data_provider.dart
@@ -2,16 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:project_flutter_springboot/datasource/data_source.dart';
 import 'package:project_flutter_springboot/datasource/dummy_data_source.dart';
 
+import '../models/bus_model.dart';
 import '../models/bus_reservation.dart';
 import '../models/bus_schedule.dart';
 import '../models/but_route.dart';
+import '../models/response_model.dart';
 
 class AppDataProvider extends ChangeNotifier {
+  List<Bus> _busList = [];
+  List<BusRoute> _routeList = [];
+  List<BusReservation> _reervationList = [];
   List<BusSchedule> _scheduleList = [];
-
   List<BusSchedule> get scheduleList => _scheduleList;
-
+  List<Bus> get busList => _busList;
+  List<BusRoute> get routeList => _routeList;
+  List<BusReservation> get reservationList => _reervationList;
   final DataSource _dataSource = DummyDataSource();
+
+  Future<ResponseModel> addReservation(BusReservation reservation) {
+    return _dataSource.addReservation(reservation);
+  }
 
   Future<BusRoute?> getRouteByCityFromAndCityTo(
       String cityFrom, String cityTo) {
--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -5,10 +5,20 @@ String getFormattedDate(DateTime dt, {String pattern = 'dd/MM/yyyy'}) {
   return DateFormat(pattern).format(dt);
 }
 
+String getFormattedTime(TimeOfDay tm, {String pattern = 'HH:mm'}) {
+  return DateFormat(pattern).format(DateTime(0, 0, 0, tm.hour, tm.minute));
+}
+
 void showMsg(BuildContext context, String msg) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
       content: Text(msg),
     ),
   );
+}
+
+int getGrandTotal(int discount, int totalSeatBooked, int price, int fee) {
+  final subTotal = totalSeatBooked * price;
+  final priceAfterDiscount = subTotal - ((subTotal * discount) / 100);
+  return (priceAfterDiscount + fee).toInt();
 }
